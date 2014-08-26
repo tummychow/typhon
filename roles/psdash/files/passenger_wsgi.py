9ca5d46483d41e1bf73b8bac900f2e477da816d5
tummychow <tummychow@users.noreply.github.com>
2014-08-25 22:55:29 -0400

    Move PYTHON_EGG_CACHE to nginx configs
    We can set this with passenger_set_cgi_param as well, which makes the
    passenger WSGI script cleaner.

--- roles/psdash/files/passenger_wsgi.py
+++ roles/psdash/files/passenger_wsgi.py
@@ -1,9 +1,6 @@
 import os
 import os.path
 
-# put the egg cache in passenger's ./tmp folder
-os.environ['PYTHON_EGG_CACHE'] = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'tmp')
-
 from psdash.run import PsDashRunner
 application = PsDashRunner.create_from_args().app
 
